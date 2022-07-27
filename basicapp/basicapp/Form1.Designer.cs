5597d87194d205487d755d97245120571bf8f532
RoxareX <93204788+RoxareX@users.noreply.github.com>
2022-07-27 20:02:11 +0300

    Add files via upload

---
+++ basicapp/basicapp/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace basicapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Amount1 = new System.Windows.Forms.TextBox();
+            this.mm1_text1 = new System.Windows.Forms.TextBox();
+            this.mm1_text2 = new System.Windows.Forms.TextBox();
+            this.mm2_text2 = new System.Windows.Forms.TextBox();
+            this.mm2_text3 = new System.Windows.Forms.TextBox();
+            this.Amount2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.answer1_t1 = new System.Windows.Forms.Label();
+            this.answer1_t2 = new System.Windows.Forms.Label();
+            this.answer1_t3 = new System.Windows.Forms.Label();
+            this.mm2_text1 = new System.Windows.Forms.TextBox();
+            this.answer2_t1 = new System.Windows.Forms.Label();
+            this.answer2_t2 = new System.Windows.Forms.Label();
+            this.answer2_t3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Amount1
+            // 
+            this.Amount1.Location = new System.Drawing.Point(68, 70);
+            this.Amount1.Name = "Amount1";
+            this.Amount1.Size = new System.Drawing.Size(148, 20);
+            this.Amount1.TabIndex = 0;
+            this.Amount1.Text = "1";
+            // 
+            // mm1_text1
+            // 
+            this.mm1_text1.Location = new System.Drawing.Point(68, 114);
+            this.mm1_text1.Name = "mm1_text1";
+            this.mm1_text1.Size = new System.Drawing.Size(148, 20);
+            this.mm1_text1.TabIndex = 1;
+            // 
+            // mm1_text2
+            // 
+            this.mm1_text2.Location = new System.Drawing.Point(68, 141);
+            this.mm1_text2.Name = "mm1_text2";
+            this.mm1_text2.Size = new System.Drawing.Size(148, 20);
+            this.mm1_text2.TabIndex = 2;
+            // 
+            // mm2_text2
+            // 
+            this.mm2_text2.Location = new System.Drawing.Point(68, 368);
+            this.mm2_text2.Name = "mm2_text2";
+            this.mm2_text2.Size = new System.Drawing.Size(148, 20);
+            this.mm2_text2.TabIndex = 4;
+            // 
+            // mm2_text3
+            // 
+            this.mm2_text3.Location = new System.Drawing.Point(68, 394);
+            this.mm2_text3.Name = "mm2_text3";
+            this.mm2_text3.Size = new System.Drawing.Size(148, 20);
+            this.mm2_text3.TabIndex = 5;
+            // 
+            // Amount2
+            // 
+            this.Amount2.Location = new System.Drawing.Point(68, 297);
+            this.Amount2.Name = "Amount2";
+            this.Amount2.Size = new System.Drawing.Size(148, 20);
+            this.Amount2.TabIndex = 6;
+            this.Amount2.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 38);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(222, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(222, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(222, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "mm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(222, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "mm";
+            // 
+            // answer1_t1
+            // 
+            this.answer1_t1.AutoSize = true;
+            this.answer1_t1.ForeColor = System.Drawing.Color.White;
+            this.answer1_t1.Location = new System.Drawing.Point(112, 223);
+            this.answer1_t1.Name = "answer1_t1";
+            this.answer1_t1.Size = new System.Drawing.Size(13, 13);
+            this.answer1_t1.TabIndex = 13;
+            this.answer1_t1.Text = "0";
+            // 
+            // answer1_t2
+            // 
+            this.answer1_t2.AutoSize = true;
+            this.answer1_t2.ForeColor = System.Drawing.Color.White;
+            this.answer1_t2.Location = new System.Drawing.Point(112, 245);
+            this.answer1_t2.Name = "answer1_t2";
+            this.answer1_t2.Size = new System.Drawing.Size(13, 13);
+            this.answer1_t2.TabIndex = 14;
+            this.answer1_t2.Text = "0";
+            // 
+            // answer1_t3
+            // 
+            this.answer1_t3.AutoSize = true;
+            this.answer1_t3.ForeColor = System.Drawing.Color.White;
+            this.answer1_t3.Location = new System.Drawing.Point(112, 267);
+            this.answer1_t3.Name = "answer1_t3";
+            this.answer1_t3.Size = new System.Drawing.Size(13, 13);
+            this.answer1_t3.TabIndex = 15;
+            this.answer1_t3.Text = "0";
+            // 
+            // mm2_text1
+            // 
+            this.mm2_text1.Location = new System.Drawing.Point(68, 342);
+            this.mm2_text1.Name = "mm2_text1";
+            this.mm2_text1.Size = new System.Drawing.Size(148, 20);
+            this.mm2_text1.TabIndex = 16;
+            // 
+            // answer2_t1
+            // 
+            this.answer2_t1.AutoSize = true;
+            this.answer2_t1.ForeColor = System.Drawing.Color.White;
+            this.answer2_t1.Location = new System.Drawing.Point(112, 471);
+            this.answer2_t1.Name = "answer2_t1";
+            this.answer2_t1.Size = new System.Drawing.Size(13, 13);
+            this.answer2_t1.TabIndex = 18;
+            this.answer2_t1.Text = "0";
+            // 
+            // answer2_t2
+            // 
+            this.answer2_t2.AutoSize = true;
+            this.answer2_t2.ForeColor = System.Drawing.Color.White;
+            this.answer2_t2.Location = new System.Drawing.Point(112, 494);
+            this.answer2_t2.Name = "answer2_t2";
+            this.answer2_t2.Size = new System.Drawing.Size(13, 13);
+            this.answer2_t2.TabIndex = 19;
+            this.answer2_t2.Text = "0";
+            // 
+            // answer2_t3
+            // 
+            this.answer2_t3.AutoSize = true;
+            this.answer2_t3.ForeColor = System.Drawing.Color.White;
+            this.answer2_t3.Location = new System.Drawing.Point(112, 516);
+            this.answer2_t3.Name = "answer2_t3";
+            this.answer2_t3.Size = new System.Drawing.Size(13, 13);
+            this.answer2_t3.TabIndex = 20;
+            this.answer2_t3.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(222, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 16);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "mm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(222, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 16);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "mm";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(222, 397);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 16);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "mm";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(200, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 24;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(200, 431);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 25;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "WoodCalculator";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(284, 561);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.answer2_t3);
+            this.Controls.Add(this.answer2_t2);
+            this.Controls.Add(this.answer2_t1);
+            this.Controls.Add(this.mm2_text1);
+            this.Controls.Add(this.answer1_t3);
+            this.Controls.Add(this.answer1_t2);
+            this.Controls.Add(this.answer1_t1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Amount2);
+            this.Controls.Add(this.mm2_text3);
+            this.Controls.Add(this.mm2_text2);
+            this.Controls.Add(this.mm1_text2);
+            this.Controls.Add(this.mm1_text1);
+            this.Controls.Add(this.Amount1);
+            this.Name = "Form1";
+            this.Text = "WoodCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Amount1;
+        private System.Windows.Forms.TextBox mm1_text1;
+        private System.Windows.Forms.TextBox mm1_text2;
+        private System.Windows.Forms.TextBox mm2_text2;
+        private System.Windows.Forms.TextBox mm2_text3;
+        private System.Windows.Forms.TextBox Amount2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label answer1_t1;
+        private System.Windows.Forms.Label answer1_t2;
+        private System.Windows.Forms.Label answer1_t3;
+        private System.Windows.Forms.TextBox mm2_text1;
+        private System.Windows.Forms.Label answer2_t1;
+        private System.Windows.Forms.Label answer2_t2;
+        private System.Windows.Forms.Label answer2_t3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ basicapp/basicapp/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basicapp
+{
+    public partial class Form1 : Form
+    {
+        int LabelTop = 50;
+        int LabelTop2 = 50;
+        int LabelLeft = 350;
+        int LabelLeft2 = 450;
+        Label lbl2 = new Label();
+        Label lbl4 = new Label();
+        bool start = true;
+        bool start2 = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Calculate()
+        {
+            // Gets the users input
+            string x1 = mm1_text1.Text;
+            string y1 = mm1_text2.Text;
+            string amount1 = Amount1.Text;
+
+            try
+            {
+                float intx1 = Convert.ToInt32(x1);
+                float inty1 = Convert.ToInt32(y1);
+                float intamount1 = Convert.ToInt32(amount1);
+
+
+                // Makes the calculation
+                float calc1 = intx1 * inty1 * intamount1;
+                float calc2 = (intx1 * inty1) / 100 * intamount1;
+                float calc3 = (intx1 * inty1) / 1000000 * intamount1;
+                answer1_t1.Text = calc1.ToString() + " mm2";
+                answer1_t2.Text = calc2.ToString() + " cm2";
+                answer1_t3.Text = calc3.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please Use Numbers");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculate();
+            if (checkBox1.Checked)
+            {
+                newLabel(LabelTop, LabelLeft);
+            }
+            LabelTop = LabelTop + 21;
+        }
+
+        private void newLabel(int PosTop, int PosLeft)
+        {
+            Label lbl = new Label();
+            this.Controls.Add(lbl);
+            lbl.Top = PosTop;
+            lbl.Left = PosLeft;
+            lbl.ForeColor = Color.Gray;
+            lbl.Text = answer1_t3.Text;
+
+            this.Controls.Add(lbl2);
+            lbl2.Top = PosTop + 21;
+            lbl2.Left = PosLeft;
+            if (start == true)
+            {
+                lbl2.Text = "0";
+                start = false;
+            }
+            lbl2.ForeColor = Color.White;
+            float floatanswer = float.Parse(lbl2.Text);
+            float floatlbl = float.Parse(lbl.Text);
+            float equals = floatanswer + floatlbl;
+            lbl2.Text = equals.ToString();
+        }
+
+        public void Calculate2()
+        {
+            // Gets the user input
+            string x2 = mm2_text1.Text;
+            string y2 = mm2_text2.Text;
+            string z2 = mm2_text3.Text;
+            string amount2 = Amount2.Text;
+
+            try
+            {
+                float intx2 = Convert.ToInt32(x2);
+                float inty2 = Convert.ToInt32(y2);
+                float intz2 = Convert.ToInt32(z2);
+                float intamount2 = Convert.ToInt32(amount2);
+
+                // Makes the calcualtion
+                float calc4 = intx2 * inty2 * intz2 * intamount2;
+                float calc5 = (intx2 * inty2 * intz2) / 1000 * intamount2;
+                float calc6 = (intx2 * inty2 * intz2) / 1000000000 * intamount2;
+                answer2_t1.Text = calc4.ToString() + " mm3";
+                answer2_t2.Text = calc5.ToString() + " cm3";
+                answer2_t3.Text = calc6.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please Use Numbers");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculate2();
+            if (checkBox2.Checked)
+            {
+                newLabel2(LabelTop2, LabelLeft2);
+            }
+            LabelTop2 = LabelTop2 + 21;
+        }
+
+        private void newLabel2(int PosTop, int PosLeft)
+        {
+            Label lbl3 = new Label();
+            this.Controls.Add(lbl3);
+            lbl3.Top = PosTop;
+            lbl3.Left = PosLeft;
+            lbl3.ForeColor = Color.Gray;
+            lbl3.Text = answer2_t3.Text;
+
+            this.Controls.Add(lbl4);
+            lbl4.Top = PosTop + 21;
+            lbl4.Left = PosLeft;
+            if (start2 == true)
+            {
+                lbl4.Text = "0";
+                start2 = false;
+            }
+            lbl4.ForeColor = Color.White;
+            float floatanswer = float.Parse(lbl4.Text);
+            float floatlbl = float.Parse(lbl3.Text);
+            float equals = floatanswer + floatlbl;
+            lbl4.Text = equals.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Size = new Size(600, 600);
+            }
+            else
+            {
+                if (!checkBox2.Checked)
+                {
+                    Size = new Size(300, 600);
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                Size = new Size(600, 600);
+            }
+            else
+            {
+                if (!checkBox1.Checked)
+                {
+                    Size = new Size(300, 600);
+                }
+            }
+        }
+    }
+}
